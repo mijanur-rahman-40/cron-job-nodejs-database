c4d39c199ef0f5971a26efe6a492640417851fca
mijanur-rahman-40 <mijanurrahman31416@gmail.com>
2022-05-03 02:16:18 +0600

    updated

--- corn-job-database/app.js
+++ corn-job-database/app.js
@@ -3,10 +3,12 @@ const app = express();
 // implementing corn job
 const cron = require('node-cron');
 
-cron.schedule('* * * * * *', () => {
+// cron.schedule('* 41 * * * Sunday', () => {
+//   console.log('running a task every second');
+// });
+
+cron.schedule('* * * * Sunday', () => {
   console.log('running a task every second');
 });
-
-
 
 app.listen(5000, () => console.log("App Running on http://localhost:5000"));
