b74caeb31d38b8bfe901ed122de5244ffda44b5a
mijanur-rahman-40 <mijanurrahman31416@gmail.com>
2022-05-30 14:49:31 +0600

    templete design completed

--- corn-job-database/app.js
+++ corn-job-database/app.js
@@ -1,14 +1,27 @@
-const express = require("express");
+import express from "express";
 const app = express();
 // implementing corn job
-const cron = require('node-cron');
+import cron from 'node-cron';
 
-// cron.schedule('* 41 * * * Sunday', () => {
+
+cron.schedule('* 30 * * * Sunday', () => {
+  console.log('running a task every 30min');
+});
+
+// cron.schedule('* * * * Sunday', () => {
 //   console.log('running a task every second');
 // });
 
-cron.schedule('* * * * Sunday', () => {
+cron.schedule('* * * * * *', () => {
   console.log('running a task every second');
 });
+
+// cron.schedule('* * */3 * *', () => {
+//     console.log('that says, every minute of every hour on every three days. ');
+// });
+
+// cron.schedule('0 0 */3 * *', () => {
+//     console.log('Says at 00:00 (midnight) every three days.');
+// });
 
 app.listen(5000, () => console.log("App Running on http://localhost:5000"));
